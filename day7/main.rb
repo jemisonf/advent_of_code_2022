062f1b61c0ee650806c2233f8663f4ae88f03f90
Fischer Jemison <fischer@fgj.codes>
2022-12-07 08:02:24 -0800

    day 7 wip

---
+++ day7/main.rb
@@ -0,0 +1,102 @@
+require 'json'
+
+filename = ARGV[0]
+contents = File.open(filename).readlines
+
+tree = {"size" => 0, "/" => {"size" => 0, "name" => "/"}}
+
+cd = /\$ cd (?<dir>.+)/
+ls = /\$ ls/
+dir = /dir (?<dir>.+)/
+file = /(?<size>\d+) (?<name>.+)/
+
+current_dir = tree
+
+contents.each do |line| 
+    cdmatch = cd.match(line)
+
+    if cdmatch
+        new_dir = current_dir[cdmatch["dir"]]
+
+        new_dir[".."] = current_dir if cdmatch["dir"] != ".."
+
+        current_dir = new_dir
+        
+
+        next
+    end
+
+    lsmatch = ls.match(line)
+
+    if lsmatch
+        next
+    end
+
+    dirmatch = dir.match(line)
+
+    if dirmatch
+        # puts dirmatch
+        current_dir[dirmatch["dir"]] = {"size" => 0, "name" => dirmatch["dir"]}
+        # puts current_dir
+
+        next
+    end
+
+    filematch = file.match(line)
+
+    if filematch
+        size = filematch["size"] 
+
+        current_dir["size"] += size.to_i
+        current_dir[filematch["name"]] = size.to_i
+        next
+    end
+end
+
+dir_sizes = {}
+
+def find_files (dir)
+    files = []
+
+    dir.each do |key, value|
+        if value.is_a? Integer and key != "size"
+            files.append(value)
+        elsif value.is_a? Hash and key != ".."
+            files.concat(find_files(value))
+        end
+    end
+
+    return files
+end
+
+def compute_dir_size (dir, dir_sizes)
+    files = find_files(dir)
+    size = files.sum
+    dir_sizes[dir["name"]] = size
+    dir.each do |key, value|
+        if value.is_a? Hash and key != ".."
+            compute_dir_size(value, dir_sizes)
+        end
+    end
+end
+
+def print_tree (tree, indent_level=0)
+    puts "#{"-"*indent_level}-> #{tree["name"]} (#{find_files(tree).sum})"
+    tree.each do |key, value|
+        print_tree(value, indent_level + 2) if value.is_a? Hash and key != ".."
+    end
+
+end
+
+print_tree(tree["/"], 0)
+
+compute_dir_size(tree, dir_sizes)
+
+# puts dir_sizes
+part_1_answer = 0
+dir_sizes.each do |dir, size| 
+    part_1_answer += size if size <= 100000
+end
+
+puts part_1_answer
+
